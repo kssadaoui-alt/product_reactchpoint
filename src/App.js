68f21c603abb47d0640cb109a11a908a4148740d
karima <kssaadaoui.alt@gmail.com>
2026-01-14 22:15:23 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // 🔹 State initial
+    this.state = {
+      Person: {
+        fullName: "Lionel Messi",
+        bio: "Professional football player",
+        imgSrc: "https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg",
+        profession: "Footballer",
+      },
+      shows: false,
+      timeInterval: 0,
+    };
+  }
+
+  // 🔹 Lifecycle: component mounted
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeInterval: prevState.timeInterval + 1,
+      }));
+    }, 1000);
+  }
+
+  // 🔹 Lifecycle: component unmounted
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  // 🔹 Toggle show state
+  toggleShow = () => {
+    this.setState({ shows: !this.state.shows });
+  };
+
+  render() {
+    const { Person, shows, timeInterval } = this.state;
+
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <button onClick={this.toggleShow}>
+          {shows ? "Hide Profile" : "Show Profile"}
+        </button>
+
+        <p>⏱ Time since component mounted: {timeInterval} seconds</p>
+
+        {shows && (
+          <div>
+            <h2>{Person.fullName}</h2>
+            <img
+              src={Person.imgSrc}
+              alt="profile"
+              width="200"
+              style={{ borderRadius: "10px" }}
+            />
+            <p><strong>Bio:</strong> {Person.bio}</p>
+            <p><strong>Profession:</strong> {Person.profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
